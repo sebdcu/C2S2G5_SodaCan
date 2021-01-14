432e31894fdaed9cd08c72e405ea8d002b0ed180
Seb <sebastian.dooris2@mail.dcu.ie>
2021-01-14 19:14:10 +0000

    Added javadocs comments

--- src/SodaCan.java
+++ src/SodaCan.java
@@ -1,9 +1,26 @@
+/**
+ * This class stores the methods we need to calculate surface are and volume of a can.
+ * @author C2 Class Group 5
+ * @version 1.0
+ */
 
 public class SodaCan{
+	/**
+	 * This method calculates the surface area of a can.
+	 * @param radius Radius of the can in mm.
+	 * @param height Height of the can in mm.
+	 * @return Returns surface area as a double.
+	 */
 	public double getSurfaceArea(double radius, double height) {
 		double sa=2 * Math.PI * radius * (height + radius);
 	     return sa;
 	};
+	/**
+	 * This method calculates the volume area of a can.
+	 * @param radius Radius of the can in mm.
+	 * @param height Height of the can in mm.
+	 * @return Returns volume area as a double.
+	 */
 	public double getVolume(double radius, double height) {
 		double volume=(Math.PI * height * radius * radius);
 		return volume;
